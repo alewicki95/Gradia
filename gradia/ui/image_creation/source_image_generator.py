4a48056b121c319423458cf69f9089aad0284eb0
Alexander Vanhee <160625516+AlexanderVanhee@users.noreply.github.com>
2025-07-21 19:01:01 +0200

    add 3 extra themes

--- gradia/ui/image_creation/source_image_generator.py
+++ gradia/ui/image_creation/source_image_generator.py
@@ -240,6 +240,7 @@ class LanguageManager:
 class StyleManager:
     def __init__(self):
         self.style_manager = GtkSource.StyleSchemeManager.get_default()
+        self.style_manager.append_search_path("resource:///be/alexandervanhee/gradia/source-styles")
         self.settings = Gtk.Settings.get_default()
         self.theme_changed_callbacks = []
 
@@ -255,7 +256,10 @@ class StyleManager:
             'cobalt',
             'kate-dark',
             'oblivion',
-            'solarized-dark'
+            'solarized-dark',
+            'clone-of-ubuntu',
+            'builder-dark',
+            'vscode-dark'
         ]
 
         self.use_generic_styles = True
